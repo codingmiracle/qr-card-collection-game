3119ff52f8eef4bc51abbc2e78cac3c1f45bfdf7
David <dav.gruber@htlstp.at>
2023-05-26 20:44:52 +0200

    add: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,31 +8,37 @@ const inter = Inter({subsets: ['latin']})
 
 
 export default async function RootLayout({
-                                           children,
+                                             children,
                                          }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
 
-  const supabase = createClient();
+    const supabase = createClient();
 
-  const {
-    data: {session},
-  } = await supabase.auth.getSession();
+    const {
+        data: {session},
+    } = await supabase.auth.getSession();
 
-  const accessToken = session?.access_token || null;
+    const accessToken = session?.access_token || null;
 
-  return (
-      <html lang="en">
-      <body className={inter.className}>
-      <div className="flex min-h-screen flex-col items-center justify-center py-2">
-        <main className={'dark'}>
-          <h1 className="mb-12 text-5xl font-bold sm:text-6xl">
-            Title <span className="font-black text-green-400"> Highlight </span>
-          </h1>
-          <AuthProvider accessToken={accessToken}>{children}</AuthProvider>
-        </main>
-      </div>
-      </body>
-      </html>
-  )
+    return (
+        <html lang="en">
+        <head>
+            <title>htlel</title>
+            <link rel="icon" href="/favicon.ico" sizes="any"/>
+            <link rel="icon" href={"/icon?<generated>"} type="image/<generated>" sizes="<generated>"/>
+            <link rel="apple-touch-icon" sizes="<generated>" href={"/apple-touch-icon?<generated>"}/>
+        </head>
+        <body className={inter.className}>
+        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+            <main className={'dark'}>
+                <h1 className="mb-12 text-5xl font-bold sm:text-6xl">
+                    Title <span className="font-black text-green-400"> Highlight </span>
+                </h1>
+                <AuthProvider accessToken={accessToken}>{children}</AuthProvider>
+            </main>
+        </div>
+        </body>
+        </html>
+    )
 }
