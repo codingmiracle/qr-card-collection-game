79217dbefc566c699014899fff13b87c7310f55a
David ヅ <85552617+codingmiracle@users.noreply.github.com>
2023-06-14 22:42:08 +0200

    Add predeployment scripts (#9)
    * refactor: qr-generator
    
    * add: style for qr-codes
    
    * add: card-uploader.py
    
    ---------
    
    Co-authored-by: David <dav.gruber@htlstp.at>

---
+++ supabase/storage/config.py
@@ -0,0 +1,83 @@
+common = [
+    "BIRG",
+    "DIHE",
+    "FISC",
+    "GRAS",
+    "GRUG",
+    "HALM",
+    "HOHE",
+    "ORSO",
+    "PFEF",
+    "PREI",
+    "RZEP",
+    "SCAL",
+    "SING",
+    "STAN",
+    "STEG",
+    "TILL",
+    "TOMI",
+    "UMGE",
+    "WAID",
+    "WALC",
+    "WALT",
+]
+
+rare = [
+    "CRHA",
+    "JANE",
+    "KARF",
+    "PEHA",
+    "PRAN",
+    "RADL",
+    "STEI",
+    "WAGN",
+    "WEMA",
+    "WILL"
+]
+
+legendary = [
+    "JUNG",
+    "KURA",
+    "REBE",
+    "WIJU"
+]
+
+num_common = len(common)
+num_rare = len(rare)
+num_legendary = len(legendary)
+
+titles = {"BIRG": "I love Lithuania",
+          "CRHA": "Siehe Datenblatt",
+          "DIHE": "Motivationskaffee",
+          "FISC": "Crybaby",
+          "GRAS": "Ab in den Süden",
+          "GRUG": "Speedy Grubi",
+          "HALM": "Auszeit",
+          "HOHE": "Schachmatt",
+          "JANE": "Jägermeister",
+          "JUNG": "Herr Inspektor",
+          "KARF": "Herr der Elemente",
+          "KURA": "Agent 47uF",
+          "ORSO": "Godfather",
+          "PEHA": "Teilzeitsoldat",
+          "PFEF": "Zirkusdirektor",
+          "PRAN": "Astronaut in the Ocean",
+          "PREI": "Around the world",
+          "RADL": "War das Teststoff?",
+          "REBE": "Mathe-Mann",
+          "RZEP": "Leiterplattenspieler",
+          "SCAL": "Ethische Überlegenheit",
+          "SING": "Moneygirl ",
+          "STAN": "#Surfingbird",
+          "STEG": "Gott der Krawatten",
+          "STEI": "Under the influence",
+          "TILL": "Chilli Tilli",
+          "TOMI": "Botguy",
+          "UMGE": "Plastikkurtl",
+          "WAGN": "Rue de la gack",
+          "WAID": "Woody",
+          "WALC": "Statisch geladen",
+          "WALT": "Top Golf",
+          "WEMA": "Man in the middle � ▛ ▙",
+          "WIJU": "Julie in the Muli",
+          "WILL": "Special-Schlapfen-Ops"}
