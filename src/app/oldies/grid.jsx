71383729ddb892c1f60e66c374d6bb79558ee060
Dominik <16066692+streimi@users.noreply.github.com>
2023-06-18 13:16:13 +0200

    Olides Page
    Adding oldies page and editing global css

---
+++ src/app/oldies/grid.jsx
@@ -0,0 +1,16 @@
+function ImageGridItem({ image }) {
+    const style = {
+      gridColumnEnd: `span ${getSpanEstimate(image.width)}`,
+      gridRowEnd: `span ${getSpanEstimate(image.height)}`,
+    }
+  
+    return <img style={style} src={image.url} alt={image.alt} />
+  }
+  
+  function getSpanEstimate(size) {
+    if (size > 250) {
+      return 2
+    }
+  
+    return 1
+  }
